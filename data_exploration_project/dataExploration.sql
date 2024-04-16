d072987f395580298004b9398b379234bc03f5cf
Devarapallivamsi <54593063+Devarapallivamsi@users.noreply.github.com>
2024-04-16 15:37:46 +0530

    Update dataExploration.sql

--- data_exploration_project/dataExploration.sql
+++ data_exploration_project/dataExploration.sql
@@ -1,12 +1,15 @@
---SELECT * FROM portfolioProject..CovidDeaths ORDER BY 3,4
+--1.Data regarding covid-19 deaths and vaccinations is explored on myriad ways considering the continents, countries etc.,
+--2.This project is done using SQL SERVER 
+--3.Cautions like appropriate dtypes of columns were taken care of before diving into the exploration part. For eg., wherever mathematical operatins like computing percentage etc.,
+-- is carried out,columns are casted to float type to not miss out on accuracy.
+--4. The following query helps one view the dtypes of all columns in the table
+--  SELECT COLUMN_NAME, DATA_TYPE
+--  FROM INFORMATION_SCHEMA.COLUMNS
+--  WHERE TABLE_NAME = 'CovidDeaths'; (or CovidVaccinations)
 
---SELECT * FROM portfolioProject..CovidVaccinations ORDER BY 3,4
 
 
-
---ALTER TABLE portfolioProject..CovidDeaths
---ALTER COLUMN total_cases float;
-
+--Exploration:
 
 --Likelihood of dying when covid contracts.
 SELECT location,date,total_cases,total_deaths,(total_deaths/total_cases)*100.0 AS DeathPercentage
@@ -15,22 +18,23 @@ WHERE location = 'India'
 AND continent IS NOT NULL
 ORDER BY 1,2
 
---percentage of people got affected on the basis of date
+--Percentage of people affected on the basis of date.
 SELECT location,date,total_cases,population,(total_cases/population)*100.0 AS percentPopInfected
 FROM portfolioProject..CovidDeaths
 WHERE location = 'India'
 AND continent IS NOT NULL
 ORDER BY 1,2
 
---countries with highest infection rate compared to population
-SELECT location,population,MAX(total_cases) AS highestInfectionCount,MAX((total_cases/population))*100.0 AS 
+--Countries with highest infection rate relative to their population.
+--Note: Population is same throughout all the rows i.e, not updated with respect to deaths.(Deaths has it's own column)
+SELECT location,MAX(total_cases) AS highestInfectionCount,MAX((total_cases/population))*100.0 AS 
 percentPopInfected
 FROM portfolioProject..CovidDeaths
 WHERE continent IS NOT NULL
-GROUP BY location,population
+GROUP BY location
 ORDER BY percentPopInfected DESC
 
---Countries with highest death count
+--Countries with highest death count.
 SELECT location,MAX(CAST(total_deaths AS int)) AS totalDeathCount
 FROM portfolioProject..CovidDeaths
 WHERE continent IS NOT NULL
@@ -38,7 +42,7 @@ GROUP BY location
 ORDER BY totalDeathCount DESC
 
 
---continent wise death count
+--Continent wise death count.
 SELECT location,MAX(CAST(total_deaths AS int)) AS totalDeathCount
 FROM portfolioProject..CovidDeaths
 WHERE continent IS NULL
@@ -46,7 +50,7 @@ GROUP BY location
 ORDER BY totalDeathCount DESC
 
 
--- Gloabal numbers (without any borders)
+-- Gloabal numbers (without any borders).
 SELECT date,SUM(new_cases) as total_cases, SUM(CAST(new_deaths AS INT)) as total_deaths,
 (SUM(CAST(new_deaths AS FLOAT))/SUM(new_cases))*100 AS percentOfDeaths
 FROM 
